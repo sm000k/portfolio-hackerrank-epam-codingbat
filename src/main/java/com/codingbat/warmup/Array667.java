7dc2fd1aa432007b7d27da379b1766ce5767958f
sm000k <smok.lacki@gmail.com>
2024-03-08 00:54:09 +0100

    Array667.java added simpler version

--- src/main/java/com/codingbat/warmup/Array667.java
+++ src/main/java/com/codingbat/warmup/Array667.java
@@ -21,6 +21,18 @@ public class Array667 {
         }
         return count;
     }
+    public int array667Simpler(int[] nums) {
+        int count = 0;
+        // Note: iterate to length-1, so can use i+1 in the loop
+        for (int i=0; i < (nums.length-1); i++) {
+            if (nums[i] == 6) {
+                if (nums[i+1] == 6 || nums[i+1] == 7) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 
     public static void main(String[] args) {
         int array667 = array667(new int[]{3, 6, 6, 7});
